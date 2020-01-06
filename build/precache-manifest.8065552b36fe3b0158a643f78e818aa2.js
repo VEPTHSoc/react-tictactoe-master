8a6af29976c82467d790c6f738e8f11fd7b36b22
VEPTHSoc <42776975+VEPTHSoc@users.noreply.github.com>
2020-01-06 16:36:11 +0530

    Add files via upload

---
+++ build/precache-manifest.8065552b36fe3b0158a643f78e818aa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07923ac5cf58a730d8d426c109ea6cb7",
+    "url": "/react-tictactoe-master/index.html"
+  },
+  {
+    "revision": "536814b537c34644fa6a",
+    "url": "/react-tictactoe-master/static/css/main.b3b1cec5.chunk.css"
+  },
+  {
+    "revision": "2a61afb4f34a8572ddec",
+    "url": "/react-tictactoe-master/static/js/2.e46edb23.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/react-tictactoe-master/static/js/2.e46edb23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "536814b537c34644fa6a",
+    "url": "/react-tictactoe-master/static/js/main.e0185d96.chunk.js"
+  },
+  {
+    "revision": "09cd8014af92dc10365d",
+    "url": "/react-tictactoe-master/static/js/runtime-main.a5191f53.js"
+  }
+]);
